d44140536fb0d0bb99a3b922c14671857cda1639
Hakan Akgül <hakanakgul34@gmail.com>
2017-09-25 21:56:20 +0300

    created at 07.08.2017 (dd.mm.yyyy)

---
+++ 20170807-5Con_DoWhile/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20170807_5Con_DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Klavyeden girilen sonsuz sayıyı toplayan uygulamayı yazınız. eğer sayı dışında birşey veya 0 girilirse toplamı gösteriniz.
+
+            int girilen=0, toplam = 0;
+            do
+            {
+                Console.Write("Bir sayi giriniz : ");
+                if (int.TryParse(Console.ReadLine(),out girilen) == true) // console tarafından okunan sayı "girilen"den farklı birşey mi ?
+                {
+                    toplam += girilen; // madem evet. o zaman topla.
+                }
+                else
+                {
+                    break; // çıkgit.
+                }
+                 
+            } while (girilen != 0);
+
+            Console.Write("Girilen sayıların toplamı = {0}", toplam);
+            Console.Read();
+
+        }
+    }
+}
